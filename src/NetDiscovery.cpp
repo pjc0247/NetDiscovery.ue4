38b939671fd4247ff567867d54606e20a71539b8
Park June Chul <pjc0247@naver.com>
2017-04-05 01:48:50 +0900

    Create NetDiscovery.cpp

---
+++ src/NetDiscovery.cpp
@@ -0,0 +1,40 @@
+#include "PupperPartyVR.h"
+
+#include "NetDiscoveryImpl.h"
+
+#include "NetDiscovery.h"
+
+static NetDiscoveryImpl *netDiscoveryImpl = nullptr;
+
+void UNetDiscovery::StartDiscovering() {
+	if (IsDiscovering()) {
+		UE_LOG(LogTemp, Warning, TEXT("Already discovering"));
+		return;
+	}
+
+	netDiscoveryImpl = new NetDiscoveryImpl();
+}
+bool UNetDiscovery::EndDiscovering(bool &isServer, FString &serverIp) {
+	if (IsDiscovering() == false) {
+		UE_LOG(LogTemp, Warning, TEXT("IsDiscovering != true"));
+		return false;
+	}
+
+	auto ret = netDiscoveryImpl->EndDiscovery();
+
+	isServer = netDiscoveryImpl->IsServer();
+	serverIp = FString(ANSI_TO_TCHAR(netDiscoveryImpl->GetServerIp().c_str()));
+
+	if (netDiscoveryImpl != nullptr) {
+		delete netDiscoveryImpl;
+		netDiscoveryImpl = nullptr;
+	}
+
+	return ret;
+}
+bool UNetDiscovery::IsDiscovering() {
+	if (netDiscoveryImpl == nullptr)
+		return false;
+
+	return netDiscoveryImpl->IsDiscovering();
+}
