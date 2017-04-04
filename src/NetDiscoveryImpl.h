bd2d5fd58257097d0d4ed59b2f4b44159c463160
Park June Chul <pjc0247@naver.com>
2017-04-05 01:49:12 +0900

    Create NetDiscoveryImpl.h

---
+++ src/NetDiscoveryImpl.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <WinSock2.h>
+#include <thread>
+#include <atomic>
+#include <map>
+
+class PUPPERPARTYVR_API NetDiscoveryImpl {
+public:
+	NetDiscoveryImpl();
+	~NetDiscoveryImpl();
+
+	bool EndDiscovery();
+
+	bool IsDiscovering() const;
+	bool IsServer() const;
+	std::string GetServerIp() const;
+
+private:
+	void RecvWorker();
+	void SendWorker();
+
+	std::string GetIpFromSockAddr(const sockaddr_in &addr);
+
+private:
+	SOCKET s;
+	sockaddr_in serverAddr;
+
+	int netId;
+	std::atomic<bool> discovering;
+
+	bool iamServer;
+	std::string serverIp;
+
+	std::map<int, std::string> peers;
+};
